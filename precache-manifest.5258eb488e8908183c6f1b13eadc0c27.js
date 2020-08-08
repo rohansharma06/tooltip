a2a2f03fd7b40b05e91d1da4f4872f1bf8d4eac4
rohansharma06 <rohansharma251998@gmail.com>
2020-08-08 15:03:17 +0530

    Updates

---
+++ precache-manifest.5258eb488e8908183c6f1b13eadc0c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf1438a95ffad23ea379cc6b9bd1b0d",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "e0625c7c003f3a49c7e2",
+    "url": "/tooltip/static/css/main.723c7486.chunk.css"
+  },
+  {
+    "revision": "c4cbd6a169e9e03bcb0b",
+    "url": "/tooltip/static/js/2.a9242efb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip/static/js/2.a9242efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0625c7c003f3a49c7e2",
+    "url": "/tooltip/static/js/main.3af9ff65.chunk.js"
+  },
+  {
+    "revision": "6d81586e19be32d511a4",
+    "url": "/tooltip/static/js/runtime-main.ca1d1748.js"
+  }
+]);
